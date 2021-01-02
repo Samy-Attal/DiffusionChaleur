a16625d11029f818f96e7663aa088c32528c6cf5
votrenom <samyattal@hotmail.fr>
2021-01-02 18:10:41 +0100

    adapatation du fichier matlab

--- monodimensionnel/diffusion_1D.m
+++ monodimensionnel/diffusion_1D.m
@@ -1,18 +1,16 @@
 clear all
-clc
 
 %Appel fichier
 load('resultats.txt'); 
 
 %Affichage resultat
-size(resultats)
-t=0:1e-6:1e-6*(size(resultats,2)-1);
-t(1)
-t(end)
-size(t)
+size(resultats);
+t=0:1e-3:1e-3*(size(resultats,2)-1);
 x=1:size(resultats,1);
+
 figure(1)
 imagesc(t,x,resultats(:,2:end))
-title('1-D Diffusion chaleur');
+colorbar
+title('Diffusion 1D');
 xlabel('time samples (s)');
 ylabel('Nb segments (mm)');
