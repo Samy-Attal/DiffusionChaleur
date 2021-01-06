bc2805b9615ae2d5a17b9199cde1592dad59b6e9
votrenom <samyattal@hotmail.fr>
2021-01-06 17:41:30 +0100

    programme diffusion 2D fonctionnel

--- bidimensionnel/diffusion2D.c
+++ bidimensionnel/diffusion2D.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 
 #define dx 0.001
+#define dy 0.001
 #define dt 0.000001
 
-#define file_materiaux "materiaux.txt"
-#define file_sources "sources.txt"
-#define file_syst "syst.txt"
+#define file_materiaux "./initialisation/materiaux.txt"
+#define file_source "./initialisation/source.txt"
+#define file_syst "./initialisation/syst.txt"
 
 typedef struct {
 	char nom[10];
@@ -20,11 +21,10 @@ typedef struct {
 
 typedef struct {
     int x, y;
-    unsigned long t_micro;
+    long t_micro;
 	float temp0;
 	materiau obj;
 	source src;
-
 }syst;
 
 float** creerMat(int nb_lignes, int nb_colonnes) {
@@ -40,7 +40,7 @@ float** creerMat(int nb_lignes, int nb_colonnes) {
     return mat;
 }
 
-float*** creerEspace(int x, int y, unsigned long t){
+float*** creerEspace(int x, int y, long t){
     int i;
     float*** esp;
     esp = malloc(t * sizeof(float**));
@@ -98,33 +98,31 @@ materiau* initMatiere(char* name){
 	return mater;
 }
 
-source initSources(char* name){
-    int i, nbsrc;
-    source src;
-    FILE* file = fopen(name, "r");
+syst initSys2D(char* namesyst, char* namesrc){
+    syst s; source src; materiau* m;
+    float t_seconde;
+    int choix = 0, nb, i;
+    FILE* file = fopen(namesrc, "r");
     if(file == NULL){
         printf("erreur lors de l'ouverture du fichier\n");
         exit(2);
     }
-    if(fscanf(file, "%d %d %d %d %f", src.posx, src.posy, src.dimx, src.dimy, src.temp) != 5){
-        printf("erreur : fichier %s non conforme\n", name);
+    if(fscanf(file, "%d %d %d %d %f", &src.posx, &src.dimx, &src.posy, &src.dimy, &src.temp) != 5){
+        printf("erreur : fichier %s non conforme\n", namesrc);
         exit(3);
     }
-
-    return src;
-}
-
-syst initSys(char* name){
-    syst s; materiau* m; int nb, choix, i;
-    FILE* file = fopen(name, "r");
+    fclose(file);
+    file = fopen(namesyst, "r");
     if(file == NULL){
         printf("erreur lors de l'ouverture du fichier\n");
         exit(2);
     }
-    if(fscanf(file, "%d %d\n%d\n%f", s.x, s.y, s.t_micro, s.temp0) != 4){
-        printf("erreur : fichier %s non conforme\n", name);
+    if(fscanf(file, "%d %d %f %f", &s.x, &s.y, &t_seconde, &s.temp0) != 4){
+        printf("erreur : fichier %s non conforme\n", namesyst);
         exit(3);
     }
+    s.t_micro = t_seconde/dt;
+    printf("choix du materiau :\n");
     m = initMatiere(file_materiaux);
     nb = cptLignes(file_materiaux);
     choix = 0;
@@ -140,60 +138,71 @@ syst initSys(char* name){
         choix = readInt();
     }while(choix < 0 || choix >= nb);
     s.obj = m[choix];   
-    s.src = initSources(file_sources);
+    s.src = src;
+
     return s;
 }
 
-float*** calculChaleur(syst s) {
+float*** calculChaleur2D(syst s) {
     float*** res = creerEspace(s.x, s.y, s.t_micro);
     int x, y, t;
-
+    float alpha = s.obj.alpha;
+    float dTx, dTy;
     // initialisation a t = 0
     for(x=0;x<s.x;x++){
         for(y=0;y<s.y;y++){
             // position de la source
-            if(x > s.src.posx && x < s.src.posx + s.src.dimx && y > s.src.posy && y < s.src.posy + s.src.dimy)
+            if(x >= s.src.posx && x < s.src.posx + s.src.dimx && y >= s.src.posy && y < s.src.posy + s.src.dimy)
                 res[0][x][y] = s.src.temp;
             else 
                 res[0][x][y] = s.temp0;
         }
     }
-
     for(t=1;t<s.t_micro;t++){
         for(x=0;x<s.x;x++){
             for(y=0;y<s.y;y++){
-                if(!x || x == s.x-1 || !y || y == s.y-1){
+                if((x == 0 || x == s.x-1) || (y == 0 || y == s.y-1)){ // effets de bord - condition aux limites
                     res[t][x][y] = s.temp0;
                 } else {
-                    //res[t][x][y] = s.obj.alpha * 
+                    res[t][x][y] = res[t-1][x][y]; 
+                    dTx = (res[t-1][x+1][y] + res[t-1][x-1][y] - 2*res[t-1][x][y]) / (dx * dx);
+                    dTy = (res[t-1][x][y+1] + res[t-1][x][y-1] - 2*res[t-1][x][y]) / (dy * dy); 
+                    res[t][x][y] += dt*(alpha * (dTx + dTy)); 
+                    /*
+                    if(x >= s.src.posx && x < s.src.posx + s.src.dimx && y >= s.src.posy && y < s.src.posy + s.src.dimy)
+                        res[t][x][y] += s.src.temp;*/
                 }
             }
         }
     }
+
     return res;
 }
 
-void writeCalc(char* filename, float*** calcul, unsigned long temp, int x, int y){
-    int i,j,t;
-    FILE* file = fopen(filename, "w");
-    if(file == NULL){
-        printf("erreur lors de l'ouverture du fichier\n");
-        exit(2);
-    }
-    for(t=0;t<temp;t++){ // augmenter incrementation pr diminuer ecriture inutile
-        for(i=0;i<x;i++){
-            for(j=0;j<y;j++){
-                fprintf(file, "%.0f ", calcul[t][i][j]);
-            }
+void writeFiles(syst s){
+    float*** calcul = calculChaleur2D(s);
+    int i=0,j,k,f=0;;
+    FILE* file;
+    int nbfiles = s.t_micro / 100000; //afin d'avoir 10 "images" pour 1 seconde
+    printf("nombre de fichier : %d\n", nbfiles);
+    char name[10] = "image0.txt";
+    while(f < nbfiles) {
+        name[5] = f+48;
+        file = fopen(name, "w"); 
+        for(j=0;j<s.x;j++){
+            for(k=0;k<s.y;k++)
+                fprintf(file, "%.0f ", calcul[i][j][k]);
             fprintf(file, "\n");
         }
-        fprintf(file, "\n");
-    }
+        i = i+nbfiles; 
     fclose(file);
+    f++;
+    }
 }
 
 int main(){	
-
-
+    syst pcb = initSys2D(file_syst, file_source);
+    float*** calcul = calculChaleur2D(pcb);
+    writeFiles(pcb);
     return 0;
 }
