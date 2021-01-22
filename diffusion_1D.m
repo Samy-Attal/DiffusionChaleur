e78bfe56d7583811100e46ad79c4e75032674ce0
votrenom <samyattal@hotmail.fr>
2021-01-22 10:30:00 +0100

    ajout des fichiers .m maj des txt

---
+++ diffusion_1D.m
@@ -0,0 +1,17 @@
+function diffusion_1D
+%Appel fichier
+resultats = load('./resultat1D/resultats.txt'); 
+
+%Affichage resultat
+size(resultats);
+t=0:1e-3:1e-3*(size(resultats,2)-1);
+x=1:size(resultats,1);
+
+figure(1)
+imagesc(t,x,resultats(:,2:end))
+colorbar
+title('Diffusion 1D');
+xlabel('time samples (s)');
+ylabel('Nb segments (mm)');
+
+end 
---
+++ diffusion_2D.m
@@ -0,0 +1,60 @@
+function diffusion_2D %(nomMateriau)
+
+rep='resultats2D';
+
+% lecture format data
+fileID = fopen(fullfile('.','initialisation','syst.txt'),'rt');
+metaData=fscanf(fileID,'%f');  % utiliser fgetl pour de la chaine de caractere
+dimx=metaData(1); % 1ere dim materiau
+dimy=metaData(2); % 2eme dim materiau
+Delta_t=metaData(3); % durée temporelle simu
+Tinit=metaData(4); % temperature initiale
+
+
+
+% init film avi
+v = VideoWriter('newfile.avi');
+v.FrameRate=1;
+open(v);
+
+
+% lecture fichiers de donnees
+fichtxt=dir(fullfile('.',rep,'image*.txt'))
+
+nbfich=size(fichtxt,1);
+
+for ifich=1:nbfich % boucles sur les pas de temps
+    
+     % recup fichier txt du temp ifich
+    nomfich=fichtxt(ifich).name;
+    
+     % Ouverture du fichier
+    fileID = fopen(fullfile('.',rep,nomfich),'rt');        
+
+    % lecture
+    A=fscanf(fileID,'%f'); % fichier txt-> 1 vecteur
+    Data=reshape(A,dimx,dimy).'; % reformatage des donnees 1 vecteur -> 1 matrice  ( .'=transpose NON conjugue)
+    
+    figure(1000)
+    imagesc(Data)
+    xlabel('[mm]')
+    ylabel('[mm]')
+    title('diffusion T°K 2D')
+    
+    drawnow
+    pause(0.5)
+    
+    F(ifich) = getframe;
+    writeVideo(v,F(ifich));
+end
+close(v) % fin ecriture .AVI
+
+
+% sauvegarde dans fichier de donnees matlab
+%save(fullfile('.',rep,'datamovie.mat'),'F');
+
+
+% %%%%%% recup donnees fichier matlab et lecture du film dans matlab
+% rep='images';
+% load(fullfile('.',rep,'datamovie.mat'),'F');
+% movie(F,1,1) %relecture de F 
